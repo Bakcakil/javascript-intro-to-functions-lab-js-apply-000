d3df2237e1686b2d5a201cc274bb67babcc4eff3
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-19 04:51:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,3 +45,15 @@ function logWhisper(string) {
   var b = string.toLowerCase();
   console.log(b)
 }
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
+  else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  }
+  else if (string === "I love you, Grandma.") {
+    return "I love you too."
+  }
+}
