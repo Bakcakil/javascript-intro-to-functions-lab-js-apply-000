0303c9eb9db7144b51680a56ef4ab8ebe36378d4
Berkay Akcakil <burkeakcakil@gmail.com>
2018-11-19 04:55:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+//String to uppercase//
 function shout(string) {
   return string.toUpperCase()
 }
-
+//String to lowercase//
 function whisper(string) {
   return string.toLowerCase()
 }
-
+//Console log string in uppercase//
 function logShout(string) {
   var a = string.toUpperCase();
   console.log(a)
 }
-
+//Console log string in lowercase//
 function logWhisper(string) {
   var b = string.toLowerCase();
   console.log(b)
 }
-
+//Respond to grandma if string is; lowercase, uppercase or "I love you, Grandma"//
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return "I can't hear you!"
